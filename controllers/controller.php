e28f32f45c03fc3903fc2d8e557107812984402f
Antoine Le Bras <alebras@expert-net.fr>
2022-05-09 16:03:40 +0200

    initial commit

---
+++ controllers/controller.php
@@ -0,0 +1,49 @@
+<?php
+
+$action = $_GET["action"] ?? "display";
+
+switch ($action) {
+
+  case 'register':
+    include "../views/RegisterForm.php";
+    break;
+
+  case 'logout':
+    include "../views/LogoutForm.php";
+    break;
+
+  case 'login':
+    include "../views/LoginForm.php";
+    break;
+
+  case 'newMsg':
+    // code...
+    break;
+
+  case 'newComment':
+    // code...
+    break;
+
+  case 'display':
+  default:
+    include "../models/PostManager.php";
+    $posts = GetAllPosts();
+
+    include "../models/CommentManager.php";
+    $comments = array();
+
+    // ===================HARDCODED PART===========================
+    // format idPost => array of comments
+    $comments[1] = array(
+      array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 01."),
+      array("nickname" => "FakeUser2", "created_at" => "1970-01-02 00:00:00", "content" => "Fake comment 02."),
+      array("nickname" => "FakeUser1", "created_at" => "1970-01-03 00:00:00", "content" => "Fake comment 03.")
+    );
+    $comments[3] = array(
+      array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 04."),
+    );
+    // =============================================================
+
+    include "../views/DisplayPosts.php";
+    break;
+}
